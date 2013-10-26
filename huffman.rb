eb2f344151aa0811f31782a75f17a8da95c8e030
Nguyễn Phan Hải <hainp2604@gmail.com>
2013-10-26 15:26:00 +0700

    chin

--- huffman.rb
+++ huffman.rb
@@ -1,6 +1,6 @@
 require "minitest/autorun"
 
-class TestProbTableGenerator < Minitest::Test
+class TestFreqTableGenerator < Minitest::Test
   def gen(str)
   	if str == 'a'
   		{"a" => 1}
@@ -10,7 +10,10 @@ class TestProbTableGenerator < Minitest::Test
   end
 
   def test_single_char
+  	# should return a dictionary with one key
+  	# and a value of 1 for a single-char string input
     assert_equal( {'a' => 1}, gen('a') )
     assert_equal( {'b' => 1}, gen('b') )
   end
+
 end
