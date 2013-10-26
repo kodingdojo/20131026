1a1a1e0fe0f4642df9147335d39ce3a2295c4d21
Nguyễn Phan Hải <hainp2604@gmail.com>
2013-10-26 15:34:13 +0700

    chin

--- huffman.rb
+++ huffman.rb
@@ -7,6 +7,8 @@ class TestFreqTableGenerator < Minitest::Test
   			{'a' => 2}
   		elsif str == 'b'*2
   			{'b' => 2}
+  		elsif str == 'ab'
+  			{'a' => 1, 'b' => 1}
   		end
   	else
 	  	if str == 'a'
@@ -29,6 +31,13 @@ class TestFreqTableGenerator < Minitest::Test
   	# with one key (the char) with value of 2
   	assert_equal({'a' => 2}, gen('aa'))
   	assert_equal({'b' => 2}, gen('bb'))
+  end
+
+  def test_string_with_two_different_chars
+  	# string with two different chars
+  	# => dictionary with two keys (the two chars)
+  	#    each having a value of 1
+  	assert_equal({'a' => 1, 'b' => 1}, gen("ab"))
   end
 
 end
