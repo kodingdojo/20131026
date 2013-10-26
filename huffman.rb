d24856f77bfdc303ffde6a33240b91203cd4f574
Nguyễn Phan Hải <hainp2604@gmail.com>
2013-10-26 15:29:51 +0700

    chin

--- huffman.rb
+++ huffman.rb
@@ -2,10 +2,16 @@ require "minitest/autorun"
 
 class TestFreqTableGenerator < Minitest::Test
   def gen(str)
-  	if str == 'a'
-  		{"a" => 1}
+  	if str.length == 2
+  		if str == 'aa'
+  			{'a' => 2}
+  		end
   	else
-  		{"b" => 1}
+	  	if str == 'a'
+	  		{"a" => 1}
+	  	else
+	  		{"b" => 1}
+	  	end
   	end
   end
 
@@ -14,6 +20,12 @@ class TestFreqTableGenerator < Minitest::Test
   	# and a value of 1 for a single-char string input
     assert_equal( {'a' => 1}, gen('a') )
     assert_equal( {'b' => 1}, gen('b') )
+  end
+
+  def test_doublely_duplicate_string
+  	# string with two duplicate chars => dictionary
+  	# with one key (the char) with value of 2
+  	assert_equal({'a' => 2}, gen('aa'))
   end
 
 end
